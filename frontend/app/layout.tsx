110dc4bc522e7aacde214f4ecf99c1945731c4ee
Florian Maas <fpgmaas@gmail.com>
2024-06-23 16:06:59 +0200

    better metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,7 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "PyPI Scout",
-  description: "Find PyPI packages with natural language using LLM's",
+  description: "Find Python packages on PyPI with natural language queries",
+  openGraph: {
+    title: "PyPI Scout",
+    description: "Find Python packages on PyPI with natural language queries",
+    images: [
+      {
+        url: "/pypi-light.svg",
+        width: 600,
+        height: 300,
+        alt: "pypi-scout logo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +29,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="PyPI Scout" />
+        <meta
+          property="og:description"
+          content="Find Python packages on PyPI with natural language queries"
+        />
+        <meta property="og:image" content="/pypi-light.svg" />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:image:alt" content="pypi-scout logo" />
+      </head>
       <body className={inter.className}>
         <GoogleAnalytics />
         {children}
